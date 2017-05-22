ad3e2ed84ab2763f47105b7316f08bd813b942ab
Thomas Komarnicki <tkomarnicki@hanekedesign.com>
2017-05-22 17:57:05 -0400

    start time input view

---
+++ app/src/main/java/com/github/komarnicki/thomas/ringtimer/addtimer/TimeInputView.kt
@@ -0,0 +1,13 @@
+package com.github.komarnicki.thomas.ringtimer.addtimer
+
+import android.content.Context
+import android.util.AttributeSet
+import android.view.View
+
+class TimeInputView @JvmOverloads constructor(context: Context, attrs: AttributeSet? = null, defStyleAttr: Int = 0)
+    : View(context, attrs, defStyleAttr) {
+
+    
+
+
+}
