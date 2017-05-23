2fa781afec5cc6651a780ab75fad3b41473b0959
Thomas Komarnicki <tkomarnicki@hanekedesign.com>
2017-05-23 14:15:07 -0400

    convert Timer back to kaitlin

--- app/src/main/java/com/github/komarnicki/thomas/ringtimer/addtimer/TimeInputView.kt
+++ app/src/main/java/com/github/komarnicki/thomas/ringtimer/addtimer/TimeInputView.kt
@@ -3,11 +3,26 @@ package com.github.komarnicki.thomas.ringtimer.addtimer
 import android.content.Context
 import android.util.AttributeSet
 import android.view.View
+import android.widget.FrameLayout
+import com.github.komarnicki.thomas.ringtimer.R
 
-class TimeInputView @JvmOverloads constructor(context: Context, attrs: AttributeSet? = null, defStyleAttr: Int = 0)
-    : View(context, attrs, defStyleAttr) {
+class TimeInputView : FrameLayout {
 
-    
+    constructor(context: Context, attrs: AttributeSet? = null, defStyleAttr: Int) : super(context, attrs, defStyleAttr) {
+        init()
+    }
+
+    constructor(context: Context, attrs: AttributeSet? = null) : super(context, attrs) {
+        init()
+    }
+
+    constructor(context: Context) : super(context) {
+        init()
+    }
+
+    private fun init(){
+        var view = View.inflate(context, R.layout.time_input, this)
+    }
 
 
 }
---
+++ app/src/main/java/com/github/komarnicki/thomas/ringtimer/model/Timer.kt
@@ -0,0 +1,49 @@
+package com.github.komarnicki.thomas.ringtimer.model
+
+import android.arch.persistence.room.Entity
+import android.arch.persistence.room.Ignore
+import android.arch.persistence.room.PrimaryKey
+import android.os.Parcel
+import android.os.Parcelable
+import com.github.komarnicki.thomas.ringtimer.createParcel
+
+@Entity
+class Timer : Parcelable {
+
+    @PrimaryKey(autoGenerate = true)
+    var id: Int = 0
+
+    var duration: Int = 0
+
+    var breakTime: Int = 0
+
+    @Ignore
+    constructor(duration: Int, breakTime: Int) {
+        this.duration = duration
+        this.breakTime = breakTime
+    }
+
+    constructor()
+
+    protected constructor(`in`: Parcel) {
+        id = `in`.readInt()
+        duration = `in`.readInt()
+        breakTime = `in`.readInt()
+    }
+
+    override fun describeContents(): Int {
+        return 0
+    }
+
+    override fun writeToParcel(dest: Parcel, flags: Int) {
+        dest.writeInt(id)
+        dest.writeInt(duration)
+        dest.writeInt(breakTime)
+    }
+
+    companion object {
+        @JvmField @Suppress("unused")
+        val CREATOR = createParcel {Timer(it)}
+    }
+
+}
