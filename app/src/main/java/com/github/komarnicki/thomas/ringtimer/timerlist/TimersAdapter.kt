4742622531788b30888bd5913a7a4de41260f763
Thomas Komarnicki <tkomarnicki@hanekedesign.com>
2017-05-25 18:04:43 -0400

    add second view holder type to list adapter

--- app/src/main/java/com/github/komarnicki/thomas/ringtimer/timerlist/TimersAdapter.kt
+++ app/src/main/java/com/github/komarnicki/thomas/ringtimer/timerlist/TimersAdapter.kt
@@ -8,35 +8,42 @@ import android.widget.TextView
 import com.github.komarnicki.thomas.ringtimer.R
 import com.github.komarnicki.thomas.ringtimer.model.Timer
 import io.reactivex.Observable
+import io.reactivex.disposables.Disposable
 import io.reactivex.subjects.PublishSubject
 
-class TimersAdapter(var timers: List<Timer>, var timerClickListener: TimerClickListener) : RecyclerView.Adapter<TimersAdapter.VH>() {
+class TimersAdapter(var timers: List<Timer>, var timerClickListener: TimerClickListener) : RecyclerView.Adapter<RecyclerView.ViewHolder>() {
 
-    override fun onBindViewHolder(viewHolder: VH, position: Int) {
-        val t = timers[position]
-        viewHolder.durationTime.text = displayDuration(t.duration)
-        viewHolder.breakTime.text = displayDuration(t.breakTime)
-        viewHolder.itemView.setOnClickListener {
-            // todo connect to TimerPlaybackView
-            val timerPlaybackSubject = PublishSubject.create<Boolean> {
-                it.onNext(true)
-                // todo
+    var timerPlaybackSubject: PublishSubject<Boolean>? = null
+
+    override fun onBindViewHolder(viewHolder: RecyclerView.ViewHolder, position: Int) {
+        if(viewHolder is VH) {
+            val t = timers[position]
+            viewHolder.durationTime.text = displayDuration(t.duration)
+            viewHolder.breakTime.text = displayDuration(t.breakTime)
+            viewHolder.itemView.setOnClickListener {
+                // todo connect to TimerPlaybackView
+                timerPlaybackSubject = PublishSubject.create()
+
+                timerClickListener.onTimerClicked(timers[position], it, timerPlaybackSubject!!)
             }
-
-            timerClickListener.onTimerClicked(timers[position], it, timerPlaybackSubject)
+        }else if(viewHolder is TimerPlaybackViewHolder){
+            viewHolder.timerPlaybackView.playPauseObservable.subscribe(timerPlaybackSubject)
         }
     }
 
-    override fun onCreateViewHolder(viewGroup: ViewGroup, viewType: Int): VH {
+    override fun onCreateViewHolder(viewGroup: ViewGroup, viewType: Int): RecyclerView.ViewHolder {
         return VH(LayoutInflater.from(viewGroup.context).inflate(R.layout.row_timers, viewGroup, false));
     }
 
     override fun getItemCount(): Int = timers.size
 
-
     class VH(itemView: View) : RecyclerView.ViewHolder(itemView) {
         var durationTime : TextView = itemView.findViewById(R.id.row_timer_duration) as TextView
         var breakTime : TextView = itemView.findViewById(R.id.row_timers_break) as TextView
+    }
+
+    class TimerPlaybackViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
+        var timerPlaybackView : TimerPlaybackView = itemView.findViewById(0) as TimerPlaybackView
     }
 
     fun displayDuration(seconds: Int): String {
